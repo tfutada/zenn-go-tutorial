a769154a734c128badea27604f08ae1f54b801fe
tfutada <tafutada@gmail.com>
2024-01-27 16:35:16 +0900

    Update ReadOS test to handle files
    The main_test.go file was updated to handle actual files in the TestReadOS testing function. This includes getting the file size and using a buffered reader to read the file. Additionally, a helper function for getting a file size was added.

--- src/buffer1/main_test.go
+++ src/buffer1/main_test.go
@@ -1,38 +1,72 @@
 package main
 
 import (
-    "io"
-    "strings"
-    "testing"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"testing"
 )
 
 // Your ReadOS function here...
 
 // TestReadOS tests the ReadOS function
 func TestReadOS(t *testing.T) {
-    tests := []struct {
-        name     string
-        reader   io.Reader
-        n        int
-        Fsize    int
-        expected int
-        wantErr  bool
-    }{
-        {
-            name:     "NormalRead",
-            reader:   strings.NewReader("Hello, World!"),
-            n:        1,
-            Fsize:    13,
-            expected: 13,
-            wantErr:  false,
-        },
-        // Add more test cases here...
-    }
+	fileName := "big-1mb.log"
 
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-           ReadOS(tt.reader, tt.n, tt.Fsize) 
-        })
-    }
+	f, err := os.Open(fileName)
+	defer f.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	size, err := getFileSize(fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// use bufio
+	reader := bufio.NewReader(f)
+
+	tests := []struct {
+		name     string
+		reader   io.Reader
+		n        int
+		Fsize    int
+		expected int
+		wantErr  bool
+	}{
+		{
+			name:     "NormalRead",
+			reader:   reader,
+			n:        1,
+			Fsize:    size,
+			expected: 13,
+			wantErr:  false,
+		},
+		// Add more test cases here...
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ReadOS(tt.reader, tt.n, tt.Fsize)
+		})
+	}
 }
 
+func getFileSize(fileName string) (int, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(fileInfo.Size()), nil
+}
