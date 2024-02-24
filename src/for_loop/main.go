46a631e35e1017f3ba5d79e570e7286d97e4ff24
tfutada <tafutada@gmail.com>
2024-02-24 17:38:54 +0900

    Refactor for-loop to handle goroutines
    The previous implementation in the main function was simplified by removing unnecessary time sleeps and adjusting the for-loop to directly handle goroutines. Now, the synchronization waitgroup adds one at each iteration, creating a new goroutine that immediately gets executed, printing the current iteration number and its address.

--- src/for_loop/main.go
+++ src/for_loop/main.go
@@ -3,23 +3,18 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(1)
 
-	go func() {
-		defer wg.Done()
-
-		time.Sleep(time.Second * 5)
-		fmt.Println("Hello")
-	}()
-
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-		time.Sleep(time.Millisecond * 100)
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(i)
+			fmt.Printf("Address of i: %p\n", &i)
+		}()
 	}
 
 	wg.Wait()
