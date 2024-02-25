eaa9fc14372ea9a07fd1a300fff9b4c3550838b5
tfutada <tafutada@gmail.com>
2024-02-25 11:15:38 +0900

    Simplify main function in capacity/main.go
    Removed the for loop and counter that were previously used. Now, the main function only creates a slice with a specified length and capacity, appends a zero to it, and prints the length-capacity tuple. This gives a direct and clearer picture of the slice's length and capacity.

--- src/capacity/main.go
+++ src/capacity/main.go
@@ -3,14 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x2Counter := 0
-	for i := 0; i < 1000; i++ {
-		slice := make([]int, i, i)
-		slice = append(slice, 0)
-		if cap(slice) == i*2 {
-			x2Counter++
-			println(i)
-		}
-	}
-	fmt.Println(x2Counter)
+	slice := make([]int, 336, 336)
+	slice = append(slice, 0)
+	fmt.Printf("len=%d, cap=%d", len(slice), cap(slice))
 }
