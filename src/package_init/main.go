ba7c492bbcfebfc07fec28451771b3d7ce1520c7
tfutada <tafutada@gmail.com>
2024-03-08 21:23:35 +0900

    import .

--- src/package_init/main.go
+++ src/package_init/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"log"
+	. "math"
 	_ "tutorial1/src/package_init/lab"
 )
 
 func main() {
 	log.Println("main function")
+
+	// calc an area of a circle with math pi.
+	area := Pi * 9
+	log.Println("area of a circle:", area)
 }
