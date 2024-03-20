c530bef9739eeab82822f22898ee564e4a9c4082
tfutada <tafutada@gmail.com>
2024-03-20 11:38:07 +0900

    http server

--- src/goroutine2/main.go
+++ src/goroutine2/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"runtime/trace"
 	"time"
 )
 
 func main() {
+	f, err := os.Create("trace2.out")
+	if err != nil {
+		log.Fatalf("failed to create trace output file: %v", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("failed to close trace file: %v", err)
+		}
+	}()
+
+	// Start the trace
+	if err := trace.Start(f); err != nil {
+		log.Fatalf("failed to start trace: %v", err)
+	}
+	defer trace.Stop()
+
 	ch := make(chan string, 2)  // Create a channel of type string.
 	go processTask(ch)          // Start a goroutine that executes processTask.
 	time.Sleep(5 * time.Second) // Simulate a task taking 2 seconds.
--- src/http_mux/main.go
+++ src/http_mux/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func itemHandler(w http.ResponseWriter, r *http.Request) {
-
 	id := r.PathValue("id") // Dynamically access the path variable
 	fmt.Fprintf(w, "Retrieving item with ID: %s", id)
+}
+
+func delayHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(3 * time.Second) // Sleep for 2 seconds
+	fmt.Fprint(w, "Delay complete")
+
 }
 
 func filesHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +27,7 @@ func main() {
 
 	mux.HandleFunc("/items/{id}", itemHandler)
 	mux.HandleFunc("/files/{path...}", filesHandler)
+	mux.HandleFunc("/delay", delayHandler)
 
 	http.ListenAndServe(":8080", mux)
 }
