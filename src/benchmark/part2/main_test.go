15abd8cd85966c89ae4419f39ed8b8e16af75cff
tfutada <tafutada@gmail.com>
2024-04-05 12:11:45 +0900

    go bench part2

---
+++ src/benchmark/part2/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+var sink int
+
+func BenchmarkSolution(b *testing.B) {
+	res := 0
+
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		b.Fatal(err) // Use b.Fatal to stop the benchmark in case of an error
+	}
+	inputStr := string(input)
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	println(b.N)
+	for i := 0; i < b.N; i++ {
+		res = Solve(inputStr, 80)
+	}
+
+	sink = res
+	println(sink)
+}
