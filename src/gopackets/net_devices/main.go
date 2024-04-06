f99b2cc5b669e3cbdc24ab12cc681da7d682f866
tfutada <tafutada@gmail.com>
2024-04-06 09:33:48 +0900

    packet capture

---
+++ src/gopackets/net_devices/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatalf("error retrieving devices - %v", err)
+	}
+
+	for _, device := range devices {
+		fmt.Printf("Device Name: %s\n", device.Name)
+		fmt.Printf("Device Description: %s\n", device.Description)
+		fmt.Printf("Device Flags: %d\n", device.Flags)
+		for _, iaddress := range device.Addresses {
+			fmt.Printf("\tInterface IP: %s\n", iaddress.IP)
+			fmt.Printf("\tInterface NetMask: %s\n", iaddress.Netmask)
+		}
+	}
+}
