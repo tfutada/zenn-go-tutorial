700cb8462320785aa4006124ed592a349fe405a2
tfutada <tafutada@gmail.com>
2024-04-26 13:03:11 +0900

    rune. grapheme

--- src/string2/main.go
+++ src/string2/main.go
@@ -40,12 +40,20 @@ func stringPointer4() {
 	fmt.Println("runes: ", utf8.RuneCountInString(tmp)) // prints: 1
 }
 
+// Go では rune は int32 のエイリアスです。rune は Unicode のコードポイントを表します。
+// 一部の絵文字は複数の rune で構成されています。
 func stringPointer5() {
 	tmp := "❤€%……&*"
 	fmt.Printf("char at 0 index, has type %T and value is %+v\n", tmp[0], tmp[0])
 
+	// loop over the Rune. int32.
+	// Runeごとでループすると、絵文字(複数のRune)が分断されることがある。
 	for _, t := range tmp {
 		fmt.Printf("value is %+v type is %T\n", t, t)
 
 	}
+
+	// https://chimpan.hateblo.jp/entry/2022/04/06/212011
+	// Grapheme を使うと、マルチ・ユニコード絵文字を正しく扱うことができる。?
+	// https://github.com/rivo/uniseg
 }
