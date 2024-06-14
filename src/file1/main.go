f3b138e43a159283e209ad7ae24e8b2a532be251
tfutada <tafutada@gmail.com>
2024-06-14 15:08:33 +0800

    rename file

--- src/file1/main.go
+++ src/file1/main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
-	f, err := os.Open("foo.txt")
+	f, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -16,22 +17,26 @@ func main() {
 
 	for {
 		data := make([]byte, 10)
-	
+
 		count, err := f.Read(data)
 		if err != nil {
 			fmt.Println(err)
 			if err == io.EOF {
 				break
 			}
+			break
 		}
-
+		println(count)
 		totalCount += count
 	}
 
+	var reader io.Reader = strings.NewReader(" テストデータ")
+	var readCloser io.ReadCloser = io.NopCloser(reader)
+
 	fmt.Printf("count %v\n", totalCount)
+	fmt.Printf("%#v", readCloser)
 }
 
 // in Rust, you need to provide a buffer to receive file contents, but no need to specify the size.
 // let mut contents = String::new();
 //    file.read_to_string(&mut contents)?;
-
